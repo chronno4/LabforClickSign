9c8ee358d900487bf8ff2f0fb968fb3f585a34ac
chronno4 <daniel.ricci@live.com>
2022-11-28 15:18:14 -0300

    mundaça na estrutura do ECR de dev para prd

--- iac/ecr.tf
+++ iac/ecr.tf
@@ -4,25 +4,25 @@
 # nginx MS #
 #####################################
 #########
-# Develop
+#
 #########
-resource "aws_ecr_repository" "nginx-app-snapshot" {
-  name                 = "nginx-app-snapshot"
+resource "aws_ecr_repository" "nginx-app" {
+  name                 = "nginx-app-${var.ecr_registry_type[var.Env]}"
   image_tag_mutability = "MUTABLE" # SNAPSHOT
   image_scanning_configuration {
     scan_on_push = false
   }
 }
 # Em Principal informe as contas AWS que devem possuir acesso ao repositorio
-# Account ID 233801601735 (SHARED) deve estar em todos os repositorios
-resource "aws_ecr_repository_policy" "nginx-app-snapshot" {
-  repository = aws_ecr_repository.nginx-app-snapshot.name
+
+resource "aws_ecr_repository_policy" "nginx-app" {
+  repository = aws_ecr_repository.nginx-app.name
   policy     = <<EOF
 {
    "Version": "2012-10-17",
    "Statement": [
        {
-           "Sid": "AllowPushPull",
+           "Sid": "adds full ecr access to the ${var.ecr_registry_type[var.Env]} repository",
            "Effect": "Allow",
            "Principal": {
              "AWS": [
@@ -44,10 +44,9 @@ resource "aws_ecr_repository_policy" "nginx-app-snapshot" {
 EOF
 }
 
-# Remoção de imagens sem tag após 14 dias
-# Essa regra atende 99% dos cenários, só mude se for realmente necessário.
-resource "aws_ecr_lifecycle_policy" "nginx-app-snapshot" {
-  repository = aws_ecr_repository.nginx-app-snapshot.name
+
+resource "aws_ecr_lifecycle_policy" "nginx-app" {
+  repository = aws_ecr_repository.nginx-app.name
 
   policy = <<EOF
 {
@@ -66,69 +65,6 @@ resource "aws_ecr_lifecycle_policy" "nginx-app-snapshot" {
            }
        }
    ]
-}
-EOF
-}
-#########
-# Release
-#########
-resource "aws_ecr_repository" "nginx-app" {
-  name                 = "nginx-app"
-  image_tag_mutability = "IMMUTABLE" # RELEASE
-  image_scanning_configuration {
-    scan_on_push = false
-  }
-}
-
-resource "aws_ecr_repository_policy" "nginx-app" {
-  repository = aws_ecr_repository.nginx-app.name
-  policy     = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "AllowPushPull",
-            "Effect": "Allow",
-            "Principal": {
-              "AWS": [
-                "arn:aws:iam::788615289429:root"
-              ]
-            },
-            "Action": [
-                "ecr:BatchCheckLayerAvailability",
-                "ecr:BatchGetImage",
-                "ecr:CompleteLayerUpload",
-                "ecr:GetDownloadUrlForLayer",
-                "ecr:InitiateLayerUpload",
-                "ecr:PutImage",
-                "ecr:UploadLayerPart"
-            ]
-        }
-    ]
-}
-EOF
-}
-
-resource "aws_ecr_lifecycle_policy" "nginx-app" {
-  repository = aws_ecr_repository.nginx-app.name
-
-  policy = <<EOF
-{
-    "rules": [
-        {
-            "rulePriority": 1,
-            "description": "Expire images older than 14 days",
-            "selection": {
-                "tagStatus": "untagged",
-                "countType": "sinceImagePushed",
-                "countUnit": "days",
-                "countNumber": 14
-            },
-            "action": {
-                "type": "expire"
-            }
-        }
-    ]
 }
 EOF
 }
@@ -139,25 +75,25 @@ EOF
 # rails MS #
 #####################################
 #########
-# Develop
+
 #########
-resource "aws_ecr_repository" "rails-app-snapshot" {
-  name                 = "rails-app-snapshot"
+resource "aws_ecr_repository" "rails-app" {
+  name                 = "rails-app-${var.ecr_registry_type[var.Env]}"
   image_tag_mutability = "MUTABLE" # SNAPSHOT
   image_scanning_configuration {
     scan_on_push = false
   }
 }
-# Em Principal informe as contas AWS que devem possuir acesso ao repositorio
-# Account ID 233801601735 (SHARED) deve estar em todos os repositorios
-resource "aws_ecr_repository_policy" "rails-app-snapshot" {
-  repository = aws_ecr_repository.rails-app-snapshot.name
+
+
+resource "aws_ecr_repository_policy" "rails-app" {
+  repository = aws_ecr_repository.rails-app.name
   policy     = <<EOF
 {
    "Version": "2012-10-17",
    "Statement": [
        {
-           "Sid": "AllowPushPull",
+           "Sid": "adds full ecr access to the ${var.ecr_registry_type[var.Env]} repository",
            "Effect": "Allow",
            "Principal": {
              "AWS": [
@@ -179,10 +115,8 @@ resource "aws_ecr_repository_policy" "rails-app-snapshot" {
 EOF
 }
 
-# Remoção de imagens sem tag após 14 dias
-# Essa regra atende 99% dos cenários, só mude se for realmente necessário.
-resource "aws_ecr_lifecycle_policy" "rails-app-snapshot" {
-  repository = aws_ecr_repository.rails-app-snapshot.name
+resource "aws_ecr_lifecycle_policy" "rails-app" {
+  repository = aws_ecr_repository.rails-app.name
 
   policy = <<EOF
 {
@@ -201,69 +135,6 @@ resource "aws_ecr_lifecycle_policy" "rails-app-snapshot" {
            }
        }
    ]
-}
-EOF
-}
-#########
-# Release
-#########
-resource "aws_ecr_repository" "rails-app" {
-  name                 = "rails-app"
-  image_tag_mutability = "IMMUTABLE" # RELEASE
-  image_scanning_configuration {
-    scan_on_push = false
-  }
-}
-
-resource "aws_ecr_repository_policy" "rails-app" {
-  repository = aws_ecr_repository.rails-app.name
-  policy     = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "AllowPushPull",
-            "Effect": "Allow",
-            "Principal": {
-              "AWS": [
-                "arn:aws:iam::788615289429:root"
-              ]
-            },
-            "Action": [
-                "ecr:BatchCheckLayerAvailability",
-                "ecr:BatchGetImage",
-                "ecr:CompleteLayerUpload",
-                "ecr:GetDownloadUrlForLayer",
-                "ecr:InitiateLayerUpload",
-                "ecr:PutImage",
-                "ecr:UploadLayerPart"
-            ]
-        }
-    ]
-}
-EOF
-}
-
-resource "aws_ecr_lifecycle_policy" "rails-app" {
-  repository = aws_ecr_repository.rails-app.name
-
-  policy = <<EOF
-{
-    "rules": [
-        {
-            "rulePriority": 1,
-            "description": "Expire images older than 14 days",
-            "selection": {
-                "tagStatus": "untagged",
-                "countType": "sinceImagePushed",
-                "countUnit": "days",
-                "countNumber": 14
-            },
-            "action": {
-                "type": "expire"
-            }
-        }
-    ]
 }
 EOF
 }
